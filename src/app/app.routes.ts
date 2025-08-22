d7c04bc91f9dca6b90c28276bb57bf70bb14d352
alison <adevpr11@gmail.com>
2025-08-22 10:27:46 -0300

    feat: adiciona rota para o componente de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CadastrarTarefas } from './components/pages/cadastrar-tarefas/cadastrar-tarefas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'cadastar-tarefas', component:CadastrarTarefas
+    },
+    {
+      path:'', pathMatch:'full',component:CadastrarTarefas  
+    }
+];
