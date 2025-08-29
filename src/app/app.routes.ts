6734e8d10905d1629250395813b24fcd9acd00c2
alison <adevpr11@gmail.com>
2025-08-29 12:26:55 -0300

    fix: alterações no funcionamento do menu dropdown

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { CadastrarTarefas } from './components/pages/cadastrar-tarefas/cadastrar-tarefas';
+import { ConsultarTarefas } from './components/pages/consultar-tarefas/consultar-tarefas';
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
 export const routes: Routes = [
-    {
-        path:'cadastar-tarefas', component:CadastrarTarefas
-    },
-    {
-      path:'', pathMatch:'full',component:CadastrarTarefas  
-    }
+  { path: 'pages', children: [
+    { path: 'dashboard', component: Dashboard },
+    { path: 'cadastrar-tarefas', component: CadastrarTarefas },
+    { path: 'consultar-tarefas', component: ConsultarTarefas },
+  ]},
+  { path: '', redirectTo: '/pages/dashboard', pathMatch: 'full' }
 ];
