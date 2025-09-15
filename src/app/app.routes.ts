cbb9c98a754388027f98a140316cbd1976391b5f
alison <adevpr11@gmail.com>
2025-09-15 10:20:00 -0300

    chore: adiciona mapeamento de rota para components criar-usuario e autenticar-usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { CadastrarTarefas } from './components/pages/cadastrar-tarefas/cadastrar-tarefas';
 import { ConsultarTarefas } from './components/pages/consultar-tarefas/consultar-tarefas';
 import { Dashboard } from './components/pages/dashboard/dashboard';
+import { CriarUsuario } from './components/pages/criar-usuario/criar-usuario';
+import { AutenticarUsuario } from './components/pages/autenticar-usuario/autenticar-usuario';
 
 export const routes: Routes = [
   { path: 'pages', children: [
     { path: 'dashboard', component: Dashboard },
     { path: 'cadastrar-tarefas', component: CadastrarTarefas },
     { path: 'consultar-tarefas', component: ConsultarTarefas },
+    { path: 'criar-usuario', component: CriarUsuario },
+    { path: 'autenticar-usuario', component: AutenticarUsuario },
   ]},
   { path: '', redirectTo: '/pages/dashboard', pathMatch: 'full' }
 ];
