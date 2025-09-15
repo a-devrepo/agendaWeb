9f2f99496ea8da8784ea2588582cfc377cc2afa9
alison <adevpr11@gmail.com>
2025-09-15 11:49:51 -0300

    feat: adiciona estrutura de template ao component criar-usuario

--- src/app/components/pages/criar-usuario/criar-usuario.ts
+++ src/app/components/pages/criar-usuario/criar-usuario.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-criar-usuario',
-  imports: [],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule, 
+    RouterModule,
+    RouterLink
+  ],
   templateUrl: './criar-usuario.html',
   styleUrl: './criar-usuario.css'
 })
 export class CriarUsuario {
 
+  http = inject(HttpClient);
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, ]),
+    confirmarSenha: new FormControl('', [Validators.required, ])
+  });
+
+  
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Login enviado:', this.form.value);
+    }
+  }
 }
