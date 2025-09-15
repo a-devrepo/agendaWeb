93d23dff190414b18a0343b0f9fe6e456f4341c7
alison <adevpr11@gmail.com>
2025-09-15 11:44:12 -0300

    feat: adiciona estrutura de template ao component autenticar-usuario

--- src/app/components/pages/autenticar-usuario/autenticar-usuario.ts
+++ src/app/components/pages/autenticar-usuario/autenticar-usuario.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-autenticar-usuario',
-  imports: [],
+  imports: [
+    RouterLink,
+    CommonModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    RouterModule],
   templateUrl: './autenticar-usuario.html',
   styleUrl: './autenticar-usuario.css'
 })
 export class AutenticarUsuario {
 
+  http = inject(HttpClient);
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, ]),
+  });
+
+  
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Login enviado:', this.form.value);
+      // Aqui você chamaria o AuthService para autenticar
+    }
+  }
 }
