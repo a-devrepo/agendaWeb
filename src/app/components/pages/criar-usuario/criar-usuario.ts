bb0ab312a2bd9e0a8c9c90e60b2d60be6f804296
alison <adevpr11@gmail.com>
2025-09-16 12:22:31 -0300

    feat: alterações no estilo de criar-usuario e na lógica do método que faz a requisição

--- src/app/components/pages/criar-usuario/criar-usuario.ts
+++ src/app/components/pages/criar-usuario/criar-usuario.ts
@@ -1,35 +1,77 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RouterLink, RouterModule } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import { Notification } from '../../shared/notification/notification';
 
 @Component({
   selector: 'app-criar-usuario',
   imports: [
-    CommonModule, 
-    ReactiveFormsModule, 
+    CommonModule,
+    ReactiveFormsModule,
     RouterModule,
-    RouterLink
+    RouterLink,
+    Notification
   ],
   templateUrl: './criar-usuario.html',
   styleUrl: './criar-usuario.css'
 })
 export class CriarUsuario {
 
+  mensagemSucesso: string | null = null;
+  mensagemErro: string | null = null;
+
   http = inject(HttpClient);
 
-  form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    senha: new FormControl('', [Validators.required, ]),
-    confirmarSenha: new FormControl('', [Validators.required, ])
-  });
+  senhasIguaisValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const group = control as FormGroup;
+    const senha = group.get('senha')?.value;
+    const confirmarSenha = group.get('confirmarSenha')?.value;
+    return senha === confirmarSenha ? null : { senhasDiferentes: true };
+  };
 
-  
+  form = new FormGroup({
+    nome: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(100),
+      Validators.pattern(/^[A-Za-zÀ-Üà-ü\s]{6,100}$/)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9])(?!.*\s)(?!.*(.).*\\1).{8,}$/)
+    ]),
+    confirmarSenha: new FormControl('', [
+      Validators.required
+    ])
+  }, { validators: this.senhasIguaisValidator }
+);
+
 
   onSubmit() {
     if (this.form.valid) {
-      console.log('Login enviado:', this.form.value);
+
+      this.mensagemSucesso = null;
+      this.mensagemErro = null;
+      const usuario = this.form.getRawValue();
+
+      this.http.post(`${environment.apiUsuarios}/criar`, usuario)
+        .subscribe({
+          next: (response:any) =>{
+            this.mensagemSucesso = 'Usuário criado com sucesso!';
+            this.form.reset();
+          },
+          error: (e) =>{
+            const mensagem = e.error?.message || 'Erro desconhecido no servidor';
+            this.mensagemErro = mensagem;
+          }
+        });
     }
   }
 }
