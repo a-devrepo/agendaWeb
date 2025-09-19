f686c87308b98751c7c4c7ce0b6e10ee1fba41dc
alison <adevpr11@gmail.com>
2025-09-19 16:10:12 -0300

    chore: altera import do AuthGuard para as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import { ConsultarTarefas } from './components/pages/consultar-tarefas/consultar
 import { Dashboard } from './components/pages/dashboard/dashboard';
 import { CriarUsuario } from './components/pages/criar-usuario/criar-usuario';
 import { AutenticarUsuario } from './components/pages/autenticar-usuario/autenticar-usuario';
-import { authGuard } from './guards/auth-guard';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
     path: 'pages', children: [
-      { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
-      { path: 'cadastrar-tarefas', component: CadastrarTarefas, canActivate: [authGuard] },
-      { path: 'consultar-tarefas', component: ConsultarTarefas, canActivate: [authGuard] },
+      { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] },
+      { path: 'cadastrar-tarefas', component: CadastrarTarefas, canActivate: [AuthGuard] },
+      { path: 'consultar-tarefas', component: ConsultarTarefas, canActivate: [AuthGuard] },
       { path: 'criar-usuario', component: CriarUsuario },
       { path: 'autenticar-usuario', component: AutenticarUsuario },
     ]
