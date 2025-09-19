7636522aef8c7dfdd3c509d2a60738f0f28f61ff
alison <adevpr11@gmail.com>
2025-09-19 18:33:49 -0300

    feat: implementa função logout no componente navbar

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink} from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink} from '@angular/router';
+import CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-navbar',
@@ -12,4 +13,32 @@ import { RouterLink} from '@angular/router';
 })
 export class Navbar {
 
+  router = inject(Router);
+
+  nomeUsuario = signal('');
+  emailUsuario = signal('');
+
+  ngOnInit(){
+
+    const auth = sessionStorage.getItem('auth');
+
+    if(auth){
+      const bytes = CryptoJS.AES.decrypt(auth, 'auth');
+      const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+      const usuario = JSON.parse(decryptedData);
+
+      console.log(usuario)
+      this.nomeUsuario.set(usuario.nome);
+      this.emailUsuario.set(usuario.email);
+    }
+  }
+
+  logout(){
+    if(confirm('Deseja realmente sair do sistema?')){
+      
+      sessionStorage.removeItem('auth');
+
+      this.router.navigate(['/pages/dashboard']);
+    }
+  }
 }
